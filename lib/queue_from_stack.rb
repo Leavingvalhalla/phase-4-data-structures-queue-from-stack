b5d84d7c11af982dbd7ba9539a457561e6f78656
Zach Lefler <leaving.valhalla@gmail.com>
2022-05-07 14:31:27 -0700

    commit tree b9c787418e93b6af56676913fe1a94f2e2c99bc6

--- lib/queue_from_stack.rb
+++ lib/queue_from_stack.rb
@@ -1,3 +1,49 @@
 require_relative './stack'
 
-# your code here
+class MyQueue
+
+    def initialize
+        @s1 = Stack.new
+        @s2 = Stack.new
+    end
+
+    def add(value)
+        @s1.push(value)
+    end
+
+    def move_right
+        until @s1.empty? do
+            value = @s1.pop
+            @s2.push(value)
+        end
+    end
+
+    def move_left
+        until @s2.empty? do
+            value = @s2.pop
+            @s1.push(value)
+        end
+    end
+
+    def remove
+        self.move_right
+        value = @s2.pop
+        self.move_left
+        value
+    end
+
+    def peek
+        self.move_right
+        value = @s2.peek
+        self.move_left
+        value
+    end
+
+end
+
+x = MyQueue.new
+
+x.add(1)
+x.add(2)
+x.add(3)
+puts x.peek
